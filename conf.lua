c1d365d57f1db4dcea10590b6569318075b646ed
Rioni <nrosa01@ucm.es>
2023-11-21 02:13:59 +0100

    Removed unused modules

--- conf.lua
+++ conf.lua
@@ -31,22 +31,22 @@ function love.conf(t)
 	t.window.x              = nil
 	t.window.y              = nil
 
-	t.modules.audio         = true
-	t.modules.data          = true
+	t.modules.audio         = false
+	t.modules.data          = false
 	t.modules.event         = true
 	t.modules.font          = true
 	t.modules.graphics      = true
 	t.modules.image         = true
-	t.modules.joystick      = true
+	t.modules.joystick      = false
 	t.modules.keyboard      = true
-	t.modules.math          = true
+	t.modules.math          = false
 	t.modules.mouse         = true
-	t.modules.physics       = true
-	t.modules.sound         = true
-	t.modules.system        = true
-	t.modules.thread        = true
+	t.modules.physics       = false
+	t.modules.sound         = false
+	t.modules.system        = false
+	t.modules.thread        = false
 	t.modules.timer         = true
-	t.modules.touch         = true
-	t.modules.video         = true
+	t.modules.touch         = false
+	t.modules.video         = false
 	t.modules.window        = true
 end
