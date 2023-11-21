ac5b695e0c8ecd6111c22831dbb6dbc7acf7c9b6
Rioni <nrosa01@ucm.es>
2023-11-21 02:32:39 +0100

    Added build system makelove

--- src/init.lua
+++ src/init.lua
@@ -1,4 +1,4 @@
-package.path = package.path .. "./src/libs/?.lua;./src/?.lua;./src/particle_sim/?.lua"
+love.filesystem.setRequirePath(love.filesystem.getRequirePath() .. ";/src/libs/?.lua;/src/?.lua;/src/particle_sim/?.lua")
 
 -- Set filtering to nearest to avoid blurring
 love.graphics.setDefaultFilter("nearest", "nearest")
