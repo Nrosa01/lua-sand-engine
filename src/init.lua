4ba14312c09785c240053246e5c4574ecd08d3a4
Rioni <nrosa01@ucm.es>
2023-11-21 02:36:56 +0100

    Added recursive path search

--- src/init.lua
+++ src/init.lua
@@ -1,4 +1,19 @@
-love.filesystem.setRequirePath(love.filesystem.getRequirePath() .. ";/src/libs/?.lua;/src/?.lua;/src/particle_sim/?.lua")
+-- Creates a path for the require function to search for files in the src folder and subfolders recursively
+local function createPath()
+    local path = love.filesystem.getRequirePath() .. ";src/?.lua"
+    local srcPath = "src"
+    local files = love.filesystem.getDirectoryItems(srcPath)
+    for _, file in ipairs(files) do
+        local file = srcPath .. "/" .. file
+        if love.filesystem.getInfo(file, "directory") then
+        path = path .. ";" .. file .. "/?.lua"
+        end
+    end
+
+    return path
+end
+
+love.filesystem.setRequirePath(createPath())
 
 -- Set filtering to nearest to avoid blurring
 love.graphics.setDefaultFilter("nearest", "nearest")
