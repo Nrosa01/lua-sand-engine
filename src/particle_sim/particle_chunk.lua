8f6a35fd00857c44db69e9fd77937919b07a240f
Rioni <nrosa01@ucm.es>
2023-11-25 22:15:10 +0100

    Added current state to have everything relative in particle chunk

--- src/particle_sim/particle_chunk.lua
+++ src/particle_sim/particle_chunk.lua
@@ -18,119 +18,122 @@ local start_index = 0
 local end_index = 1
 
 function ParticleChunk:new(width, height)
-    local instance = {
-        matrix = {},
-        width = width,
-        height = height,
-        clock = false,
-        currentX = 0,
-        currentY = 0,
-    }
+	local instance = {
+		matrix = {},
+		width = width,
+		height = height,
+		clock = false,
+		currentX = 0,
+		currentY = 0,
+		currentIndex = 0,
+		currentType = 0,
+	}
 
-    setmetatable(instance, self)
-    return instance
+	setmetatable(instance, self)
+	return instance
 end
 
 function ParticleChunk:index(x, y)
-    return x + y * self.width
+	return x + y * self.width
 end
 
 function ParticleChunk:update(updateData)
-    local data = ParticleDefinitionsHandler.particle_data
+	local data = ParticleDefinitionsHandler.particle_data
 
-    for y = updateData.yStart, updateData.yEnd, updateData.incrementY do
-        for x = updateData.xStart, updateData.xEnd, updateData.incrementX do
-            local index = self:index(x, y)
+	for y = updateData.yStart, updateData.yEnd, updateData.incrementY do
+		for x = updateData.xStart, updateData.xEnd, updateData.incrementX do
+			local index = self:index(x, y)
 
-            if self.matrix[index].clock ~= self.clock then
-                self.matrix[index].clock = not self.clock
-            else
-                self.currentX = x
-                self.currentY = y
+			if self.matrix[index].clock ~= self.clock then
+				self.matrix[index].clock = not self.clock
+			else
+				self.currentX            = x
+				self.currentY            = y
+				self.currentIndex        = index
+				self.currentType         = self.matrix[index].type
 
-                self.matrix[index].clock = not self.clock
-                data[self.matrix[index].type].func(self)
-            end
-        end
-    end
+				self.matrix[index].clock = not self.clock
+				data[self.currentType].func(self)
+			end
+		end
+	end
 
-    self.currentX = 0
-    self.currentY = 0
-    self.clock = not self.clock
+	self.currentX = 0
+	self.currentY = 0
+	self.clock = not self.clock
 end
 
 -- Swaps particle in direction with current particle
 function ParticleChunk:swap(rx, ry)
-    local x = rx + self.currentX
-    local y = ry + self.currentY
+	local x = rx + self.currentX
+	local y = ry + self.currentY
 
-    local current_index = self:index(x, y)
-    local swap_index = self:index(self.currentX, self.currentY)
+	local current_index = self:index(x, y)
 
-    -- This doesn work because these are pointers
-    -- local copy = self.matrix[index]
-    -- self.matrix[index] = self.matrix[current_index]
-    -- self.matrix[current_index] = copy
+	-- This doesn work because these are pointers
+	-- local copy = self.matrix[index]
+	-- self.matrix[index] = self.matrix[current_index]
+	-- self.matrix[current_index] = copy
 
-    local tempType = self.matrix[current_index].type
-    local tempClock = self.matrix[current_index].clock
-    self.matrix[current_index].type = self.matrix[swap_index].type
-    self.matrix[current_index].clock = self.matrix[swap_index].clock
-    self.matrix[swap_index].type = tempType
-    self.matrix[swap_index].clock = tempClock
+	local tempType = self.matrix[current_index].type
+	local tempClock = self.matrix[current_index].clock
+	self.matrix[current_index].type = self.currentType
+	self.matrix[current_index].clock = self.matrix[self.currentIndex].clock
+	self.matrix[self.currentIndex].type = tempType
+	self.matrix[self.currentIndex].clock = tempClock
 end
 
 function ParticleChunk:setNewParticleById(rx, ry, id)
-    local x = rx + self.currentX
-    local y = ry + self.currentY
-    if x >= start_index and x <= self.width - end_index and y >= start_index and y <= self.height - end_index then
-        self.matrix[self:index(x, y)].type = id
-    end
+	local x = rx + self.currentX
+	local y = ry + self.currentY
+	if x >= start_index and x <= self.width - end_index and y >= start_index and y <= self.height - end_index then
+		self.matrix[self:index(x, y)].type = id
+	end
 end
 
 function ParticleChunk:setParticle(rx, ry, particle)
-    local x = rx + self.currentX
-    local y = ry + self.currentY
-    if x >= start_index and x <= self.width - end_index and y >= start_index and y <= self.height - end_index then
-        self.matrix[self:index(x, y)].type = particle.type
-        self.matrix[self:index(x, y)].clock = particle.clock
-    end
+	local x = rx + self.currentX
+	local y = ry + self.currentY
+	if x >= start_index and x <= self.width - end_index and y >= start_index and y <= self.height - end_index then
+		self.matrix[self:index(x, y)].type = particle.type
+		self.matrix[self:index(x, y)].clock = particle.clock
+	end
 end
 
 function ParticleChunk:getWidth()
-    return self.width
+	return self.width
 end
 
 function ParticleChunk:getHeight()
-    return self.height
+	return self.height
 end
 
 function ParticleChunk:getParticle(rx, ry)
-    local x = rx + self.currentX
-    local y = ry + self.currentY
-    return self.matrix[self:index(x, y)]
+	local x = rx + self.currentX
+	local y = ry + self.currentY
+	return self.matrix[self:index(x, y)]
 end
 
 function ParticleChunk:isInside(rx, ry)
-    local x = rx + self.currentX
-    local y = ry + self.currentY
-    return x >= start_index and x <= self.width - end_index and y >= start_index and y <= self.height - end_index
+	local x = rx + self.currentX
+	local y = ry + self.currentY
+	return x >= start_index and x <= self.width - end_index and y >= start_index and y <= self.height - end_index
 end
 
 function ParticleChunk:isEmpty(rx, ry)
-    local x = rx + self.currentX
-    local y = ry + self.currentY
-    return self:isInside(rx, ry) and self.matrix[self:index(x, y)].type == ParticleType.EMPTY
+	local x = rx + self.currentX
+	local y = ry + self.currentY
+	return self:isInside(rx, ry) and self.matrix[self:index(x, y)].type == ParticleType.EMPTY
 end
 
 function ParticleChunk:getParticleType(rx, ry)
-    if not self:isInside(rx, ry) then
-        return -1
-    end
+	if not self:isInside(rx, ry) then
+		return -1
+	end
 
-    local x = rx + self.currentX
-    local y = ry + self.currentY
-    return self.matrix[self:index(x, y)].type
+	local x = rx + self.currentX
+	local y = ry + self.currentY
+	return self.matrix[self:index(x, y)].type
 end
 
 return ParticleChunk
