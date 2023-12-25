382a141ffd59d7e7ef2eea13f7b3bf56b2e583c2
Rioni <nrosa01@ucm.es>
2023-12-25 17:18:54 +0100

    Refactor k-means clustering algorithm and optimize centroid calculations

--- src/graphics/image_utils.lua
+++ src/graphics/image_utils.lua
@@ -1,6 +1,12 @@
 local ffi = require("ffi")
 local clear = require "table.clear"
 
+-- I use in32_t because when adding the colors, they can overflow
+-- And for some reason with int16_t it doesn't work
+ffi.cdef[[
+    typedef int32_t centroid[4];
+]]
+
 -- Reescales an image to a new size
 -- If downscaled, it uses a point filter (nearest neighbor)
 -- If upscaled, it uses a linear filter
@@ -47,6 +53,15 @@ end
 
 local function distance(a, b)
     local sum = 0
+    for i = 0, 3 do
+        local diff = math.abs(a[i] - b[i])
+        sum = sum + diff
+    end
+    return sum
+end
+
+local function distance_test(a, b)
+    local sum = 0
     for i = 1, 4 do
         local diff = math.abs(a[i] - b[i])
         sum = sum + diff
@@ -55,17 +70,17 @@ local function distance(a, b)
 end
 
 local function average(clusterIndices, pixel_buffer)
-    local centroid = { 0, 0, 0, 0 }
+    local centroid = ffi.new("centroid", 0, 0, 0, 0)
     local count = 0
     for _, index in ipairs(clusterIndices) do
-        for i = 1, 4 do
-            centroid[i] = centroid[i] + pixel_buffer[index + i - 1]
+        for i = 0, 3 do
+            centroid[i] = centroid[i] + pixel_buffer[index + i]
         end
         count = count + 1
     end
 
-    for i = 1, 4 do
-        centroid[i] = centroid[i] / count
+    for i = 0, 3 do
+        centroid[i] = math.floor(centroid[i] / count)
     end
 
     return centroid
@@ -74,16 +89,12 @@ end
 
 local function distance_using_buffer(pixel_buffer, index, centroid)
     local sum = 0
-    for i = 1, 4 do
-        local diff = math.abs(pixel_buffer[index + i - 1] - centroid[i])
+    for i = 0, 3 do
+        local diff = math.abs(pixel_buffer[index + i] - centroid[i])
         sum = sum + diff
     end
     return sum
 end
-
-ffi.cdef[[
-    typedef uint8_t centroid[4];
-]]
 
 local function kmeans(pixel_buffer, buffer_size, num_clusters, sample)
     local clusters = {}
@@ -96,7 +107,7 @@ local function kmeans(pixel_buffer, buffer_size, num_clusters, sample)
     local changed = true
     while changed do
         
-        -- Refactor: Assign each pixel to the closest cluster
+        -- Assign each pixel to the closest cluster, just that
         for index = 0, buffer_size, 4 do
             local min_distance = math.huge
             local min_cluster
@@ -111,14 +122,14 @@ local function kmeans(pixel_buffer, buffer_size, num_clusters, sample)
             table.insert(min_cluster.pixels, index) -- Store the index of the pixel
         end
 
-        -- Refactor: Recalculate centroids and check for changes
+        -- Recalculate centroids and check for changes
         changed = false
         for _, cluster in ipairs(clusters) do
             local new_centroid = average(cluster.pixels, pixel_buffer)
             if distance(cluster.centroid, new_centroid) > 0.01 then
                 cluster.centroid = new_centroid
-                --cluster.pixels = {}
-                clear(cluster.pixels)
+                --cluster.pixels = {} -- This creates more tables and don't reuse memory, but it's a little faster
+                clear(cluster.pixels) -- This reuses the same table, but it's a little slower (still worthy to use, less time spent in GC)
                 changed = true
             end
         end
@@ -149,7 +160,7 @@ local function getSample(image, num_colors)
         local j = i + 1
         while j <= #sample do
             local other = sample[j]
-            if distance(color, other) < threshold then
+            if distance_test(color, other) < threshold then
                 table.remove(sample, j)
 
                 -- If table size is already num_colors, we can stop
@@ -195,6 +206,14 @@ local function getSample(image, num_colors)
         end
     end
 
+    -- Now Ill make every field a centroid
+    for _, color in ipairs(result) do
+        local centroid = ffi.new("centroid", color[1], color[2], color[3], color[4])
+        color = centroid
+    end
+
+    print("Sample size: " .. #sample)
+
     return result
 end
 
@@ -204,29 +223,25 @@ local function quantize(image, num_colors)
     local raw = ffi.cast("uint8_t*", image:getFFIPointer())
     local raw_size = width * height * 4
 
-    -- Perform k-means clustering
-    local clusters = kmeans(raw, raw_size, num_colors, getSample(image, num_colors))
-
     -- Create a new image with quantized colors
     local new_image_data = love.image.newImageData(width, height)
     local raw_new = ffi.cast("uint8_t*", new_image_data:getFFIPointer())
 
-    for index = 0, raw_size, 4 do
-        local min_distance = math.huge
-        local min_cluster
-        for _, cluster in ipairs(clusters) do
-            local dist = distance_using_buffer(raw, index, cluster.centroid)
-            if dist < min_distance then
-                min_distance = dist
-                min_cluster = cluster
-            end
-        end
+    -- Perform k-means clustering
+    local clusters = kmeans(raw, raw_size, num_colors, getSample(image, num_colors))
 
-        local color = min_cluster.centroid
-        raw_new[index] = color[1]
-        raw_new[index + 1] = color[2]
-        raw_new[index + 2] = color[3]
-        raw_new[index + 3] = color[4]
+    -- Replace each pixel with the centroid of the cluster it belongs to
+    -- I could perfectly harcode this inside the kmeans min_cluster search
+    -- But I want to keep this readable (also I measure and this didn't affect performance in a noticeable way, 
+    -- I would have hardcoded it if it did lmao)
+    for _, cluster in ipairs(clusters) do
+        local cluster_color = cluster.centroid
+        for _, index in ipairs(cluster.pixels) do
+            raw_new[index] = math.floor(cluster_color[0])
+            raw_new[index + 1] =  math.floor(cluster_color[1])
+            raw_new[index + 2] =  math.floor(cluster_color[2])
+            raw_new[index + 3] =  math.floor(cluster_color[3])
+        end
     end
 
     return new_image_data
