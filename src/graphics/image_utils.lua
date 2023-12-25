3f1247ddd4e50871cea313a4b9b9c044c9310d5b
Rioni <nrosa01@ucm.es>
2023-12-25 20:18:56 +0100

    Refactor image_utils.lua: Improve code readability and performance
    This commit refactors the `image_utils.lua` file to improve code readability and performance. Changes include:
    - Renaming the `distance` function to `distance_between_centroids` for clarity.
    - Simplifying the `average` function by removing unnecessary code.
    - Optimizing the `kmeans` function by reducing unnecessary table creations and reusing memory.
    - Improving the `getSample` function by filtering out similar colors and adding random colors if needed.
    - Enhancing the `quantize` function by optimizing the pixel replacement process.
    
    These changes aim to make the code more maintainable and efficient.

--- src/graphics/image_utils.lua
+++ src/graphics/image_utils.lua
@@ -3,7 +3,7 @@ local clear = require "table.clear"
 
 -- I use in32_t because when adding the colors, they can overflow
 -- And for some reason with int16_t it doesn't work
-ffi.cdef[[
+ffi.cdef [[
     typedef int32_t centroid[4];
 ]]
 
@@ -51,7 +51,7 @@ local function resize(image_data, new_size_x, new_size_y)
     return new_image_data
 end
 
-local function distance(a, b)
+local function distance_between_centroids(a, b)
     local sum = 0
     for i = 0, 3 do
         local diff = math.abs(a[i] - b[i])
@@ -60,23 +60,13 @@ local function distance(a, b)
     return sum
 end
 
-local function distance_test(a, b)
-    local sum = 0
-    for i = 1, 4 do
-        local diff = math.abs(a[i] - b[i])
-        sum = sum + diff
-    end
-    return sum
-end
-
 local function average(clusterIndices, pixel_buffer)
     local centroid = ffi.new("centroid", 0, 0, 0, 0)
-    local count = 0
+    local count = #clusterIndices
     for _, index in ipairs(clusterIndices) do
         for i = 0, 3 do
             centroid[i] = centroid[i] + pixel_buffer[index + i]
         end
-        count = count + 1
     end
 
     for i = 0, 3 do
@@ -99,15 +89,13 @@ end
 local function kmeans(pixel_buffer, buffer_size, num_clusters, sample)
     local clusters = {}
     for i = 1, num_clusters do
-        local colour = sample[i]
-        local centroid = ffi.new("centroid", colour[1], colour[2], colour[3], colour[4])
-        clusters[i] = { centroid = centroid, pixels = {} }
+        clusters[i] = { centroid = sample[i], pixels = {} }
     end
 
     local changed = true
     while changed do
-        
         -- Assign each pixel to the closest cluster, just that
+        -- Complexity: O(n * k) where n is the number of pixels and k is the number of clusters
         for index = 0, buffer_size, 4 do
             local min_distance = math.huge
             local min_cluster
@@ -123,10 +111,11 @@ local function kmeans(pixel_buffer, buffer_size, num_clusters, sample)
         end
 
         -- Recalculate centroids and check for changes
+        -- Complexity: O(k * n) where n is the number of pixels and k is the number of clusters
         changed = false
         for _, cluster in ipairs(clusters) do
             local new_centroid = average(cluster.pixels, pixel_buffer)
-            if distance(cluster.centroid, new_centroid) > 0.01 then
+            if distance_between_centroids(cluster.centroid, new_centroid) > 0.01 then
                 cluster.centroid = new_centroid
                 --cluster.pixels = {} -- This creates more tables and don't reuse memory, but it's a little faster
                 clear(cluster.pixels) -- This reuses the same table, but it's a little slower (still worthy to use, less time spent in GC)
@@ -148,73 +137,70 @@ local function getSample(image, num_colors)
     for y = 0, dimX - 1 do
         for x = 0, dimY - 1 do
             local r, g, b, a = resized:getPixel(x, y)
-            table.insert(sample, { r, g, b, a })
+            table.insert(sample, ffi.new("centroid", r * 255, g * 255, b * 255, a * 255))
         end
     end
 
-    -- Remove colors that are too similar
-    local i = 1
-    local threshold = 0.2
-    while i <= #sample do
-        local color = sample[i]
-        local j = i + 1
-        while j <= #sample do
-            local other = sample[j]
-            if distance_test(color, other) < threshold then
-                table.remove(sample, j)
+    local filtered_sample = {}
+    -- Given the distance formular is a-b, that means the maximum distance is 255 * 4 = 1020
+    -- If we assume that the bigger the distance the more different the colors are, and we want to keep colours that are
+    -- at least a 5% different, that means the threshold is 1020 * 0.05 = 51
+    local threshold = 51
 
-                -- If table size is already num_colors, we can stop
-                if #sample == num_colors then
-                    return sample
+    local function filter(sample, threshold)
+        -- Copy only the colors that are different enough to a new table
+        -- I don't reusae the table because calling table.remove is slow (it has to shift all the elements)
+        local filtered_sample = {}
+
+        for i = 1, #sample do
+            local color = sample[i]
+            local unique = true
+            for j = 1, #filtered_sample do
+                local other_color = filtered_sample[j]
+                if distance_between_centroids(color, other_color) < threshold then
+                    unique = false
+                    break
                 end
-            else
-                j = j + 1
+            end
+
+            if unique then
+                table.insert(filtered_sample, color)
             end
         end
-        i = i + 1
+
+        return filtered_sample
     end
 
-    -- Count the times each color appears
-    local counts = {}
-    for _, color in ipairs(sample) do
-        local count = counts[color]
-        if count then
-            counts[color] = count + 1
+    filtered_sample = filter(sample, threshold)
+
+    -- If the number of colors is less than the number of clusters, add random colors from the original sample
+    -- This is because the kmeans algorithm needs at least as many samples as clusters
+    for i = #filtered_sample + 1, num_colors do
+        local random_index = math.random(1, #sample)
+        table.insert(filtered_sample, sample[random_index])
+    end
+
+    -- Table that contains the number of times each color appears
+    -- Keys are literally the color concatenated as a string
+    -- Probably I could avoid this intermiate table if I create it before filtering the sample but whatever
+    local count = {}
+    for _, color in ipairs(filtered_sample) do
+        local key = color[0] .. color[1] .. color[2] .. color[3]
+        if count[key] then
+            count[key] = count[key] + 1
         else
-            counts[color] = 1
+            count[key] = 1
         end
     end
 
-    -- Sort the colors by frequency
-    local sorted = {}
-    for color, count in pairs(counts) do
-        table.insert(sorted, { color = color, count = count })
-    end
+    -- Thanks to the mighty power of lua supporting closures, I sort the samples by the number of times they appear
+    table.sort(filtered_sample, function(a, b)
+        local key_a = a[0] .. a[1] .. a[2] .. a[3]
+        local key_b = b[0] .. b[1] .. b[2] .. b[3]
+        return count[key_a] > count[key_b]
+    end)
 
-    table.sort(sorted, function(a, b) return a.count > b.count end)
-
-    -- Return the most frequent colors
-    local result = {}
-    for i = 1, num_colors do
-        table.insert(result, sorted[i].color)
-    end
-
-    -- Multiply each color by 255
-    for _, color in ipairs(result) do
-        for i = 1, 4 do
-            color[i] = color[i] * 255
-        end
-    end
-
-    -- Now Ill make every field a centroid
-    for _, color in ipairs(result) do
-        local centroid = ffi.new("centroid", color[1], color[2], color[3], color[4])
-        color = centroid
-    end
-
-    print("Sample size: " .. #sample)
-
-    return result
+    return filtered_sample
 end
 
 local function quantize(image, num_colors)
@@ -232,15 +218,15 @@ local function quantize(image, num_colors)
 
     -- Replace each pixel with the centroid of the cluster it belongs to
     -- I could perfectly harcode this inside the kmeans min_cluster search
-    -- But I want to keep this readable (also I measure and this didn't affect performance in a noticeable way, 
+    -- But I want to keep this readable (also I measure and this didn't affect performance in a noticeable way,
     -- I would have hardcoded it if it did lmao)
     for _, cluster in ipairs(clusters) do
         local cluster_color = cluster.centroid
         for _, index in ipairs(cluster.pixels) do
             raw_new[index] = math.floor(cluster_color[0])
-            raw_new[index + 1] =  math.floor(cluster_color[1])
-            raw_new[index + 2] =  math.floor(cluster_color[2])
-            raw_new[index + 3] =  math.floor(cluster_color[3])
+            raw_new[index + 1] = math.floor(cluster_color[1])
+            raw_new[index + 2] = math.floor(cluster_color[2])
+            raw_new[index + 3] = math.floor(cluster_color[3])
         end
     end
 
