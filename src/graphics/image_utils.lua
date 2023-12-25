d9baee848aa4f033dd145129cb312c27aff24f5c
Rioni <nrosa01@ucm.es>
2023-12-25 16:01:18 +0100

    First approach of kmeans using ffi

--- src/graphics/image_utils.lua
+++ src/graphics/image_utils.lua
@@ -1,4 +1,5 @@
 local ffi = require("ffi")
+local clear = require "table.clear"
 
 -- Reescales an image to a new size
 -- If downscaled, it uses a point filter (nearest neighbor)
@@ -53,53 +54,68 @@ local function distance(a, b)
     return sum
 end
 
-local function average(clusterIndices, pixels)
+local function average(clusterIndices, pixel_buffer)
     local centroid = { 0, 0, 0, 0 }
-    for _, pixelIndex in ipairs(clusterIndices) do
-        local pixel = pixels[pixelIndex]
+    local count = 0
+    for _, index in ipairs(clusterIndices) do
         for i = 1, 4 do
-            centroid[i] = centroid[i] + pixel[i]
+            centroid[i] = centroid[i] + pixel_buffer[index + i - 1]
         end
+        count = count + 1
     end
+
     for i = 1, 4 do
-        centroid[i] = centroid[i] / #clusterIndices
+        centroid[i] = centroid[i] / count
     end
+
     return centroid
 end
 
 
-local function kmeans(pixels, num_clusters, sample)
+local function distance_using_buffer(pixel_buffer, index, centroid)
+    local sum = 0
+    for i = 1, 4 do
+        local diff = math.abs(pixel_buffer[index + i - 1] - centroid[i])
+        sum = sum + diff
+    end
+    return sum
+end
+
+local function kmeans(pixel_buffer, buffer_size, num_clusters, sample)
     -- Initialize clusters with random pixels
+    local pixel_count = buffer_size / 4
+
     local clusters = {}
     for i = 1, num_clusters do
         clusters[i] = { centroid = sample[i], pixels = {} }
     end
 
-    print("Sample " .. #sample)
-
     local changed = true
     while changed do
-        -- Assign each pixel to the closest cluster
-        for index, pixel in ipairs(pixels) do
+        
+        -- Refactor: Assign each pixel to the closest cluster
+        for index = 0, buffer_size, 4 do
             local min_distance = math.huge
             local min_cluster
             for _, cluster in ipairs(clusters) do
-                local dist = distance(pixel, cluster.centroid)
+                local dist = distance_using_buffer(pixel_buffer, index, cluster.centroid)
                 if dist < min_distance then
                     min_distance = dist
                     min_cluster = cluster
                 end
             end
+
             table.insert(min_cluster.pixels, index) -- Store the index of the pixel
         end
 
-        -- Recalculate centroids and check for changes
+        -- Refactor: Recalculate centroids and check for changes
         changed = false
         for _, cluster in ipairs(clusters) do
-            local new_centroid = average(cluster.pixels, pixels)
-            if distance(new_centroid, cluster.centroid) > 0.01 then
+            local new_centroid = average(cluster.pixels, pixel_buffer)
+            if distance(cluster.centroid, new_centroid) > 0.01 then
                 cluster.centroid = new_centroid
-                cluster.pixels = {}
+                --cluster.pixels = {}
+                clear(cluster.pixels)
                 changed = true
             end
         end
@@ -169,39 +185,45 @@ local function getSample(image, num_colors)
         table.insert(result, sorted[i].color)
     end
 
-    -- Print the colors
+    -- Multiply each color by 255
     for _, color in ipairs(result) do
-        print(color[1], color[2], color[3], color[4])
+        for i = 1, 4 do
+            color[i] = color[i] * 255
+        end
     end
 
     return result
 end
 
-
 local function quantize(image, num_colors)
     local width, height = image:getDimensions()
-    local pixels = {}
 
-    -- Collect all pixels into a table
-    for y = 0, height - 1 do
-        for x = 0, width - 1 do
-            local r, g, b, a = image:getPixel(x, y)
-            table.insert(pixels, { r, g, b, a })
-        end
-    end
+    local raw = ffi.cast("uint8_t*", image:getFFIPointer())
+    local raw_size = width * height * 4
 
     -- Perform k-means clustering
-    local clusters = kmeans(pixels, num_colors, getSample(image, num_colors))
+    local clusters = kmeans(raw, raw_size, num_colors, getSample(image, num_colors))
 
     -- Create a new image with quantized colors
     local new_image_data = love.image.newImageData(width, height)
-    for _, cluster in ipairs(clusters) do
-        local cluster_color = cluster.centroid
-        for _, pixelIndex in ipairs(cluster.pixels) do
-            local x = (pixelIndex - 1) % width
-            local y = math.floor((pixelIndex - 1) / width)
-            new_image_data:setPixel(x, y, cluster_color[1], cluster_color[2], cluster_color[3], cluster_color[4])
+    local raw_new = ffi.cast("uint8_t*", new_image_data:getFFIPointer())
+
+    for index = 0, raw_size, 4 do
+        local min_distance = math.huge
+        local min_cluster
+        for _, cluster in ipairs(clusters) do
+            local dist = distance_using_buffer(raw, index, cluster.centroid)
+            if dist < min_distance then
+                min_distance = dist
+                min_cluster = cluster
+            end
         end
+
+        local color = min_cluster.centroid
+        raw_new[index] = color[1]
+        raw_new[index + 1] = color[2]
+        raw_new[index + 2] = color[3]
+        raw_new[index + 3] = color[4]
     end
 
     return new_image_data
